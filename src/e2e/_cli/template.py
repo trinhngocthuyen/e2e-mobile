1742b8cc8036a97c484c29902d05db2190f9fda4
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-08-13 17:30:36 +0700

    FEAT: CLI `new` to generate a new screen/simulation

--- src/e2e/_cli/template.py
+++ src/e2e/_cli/template.py
@@ -1,18 +1,39 @@
 import importlib.resources
 import shutil
+import string
+import typing as t
 
+from e2e._typing import Path
 from e2e.core.logger import logger
 
 
 class Template:
     def __init__(self, name: str) -> None:
         self.name = name
-        self.templates_dir = importlib.resources.path('e2e', '_templates')
+        self.root_templates_dir = importlib.resources.path('e2e', '_templates')
+        self.templates_dir = self.root_templates_dir / name
+
+    def copy_resource(
+        self,
+        path: Path,
+        to_path: t.Optional[Path] = None,
+        to_dir: t.Optional[Path] = None,
+        template_data: t.Optional[t.Dict[str, str]] = None,
+    ):
+        if not to_path and not to_dir:
+            raise ValueError('to_path and to_dir cannot be both None')
+        if not to_path:
+            rel_path = path.relative_to(self.templates_dir)
+            to_path = to_dir / rel_path.with_suffix('')
+        to_path.parent.mkdir(parents=True, exist_ok=True)
+        if not to_path.exists():
+            if template_data:
+                content = string.Template(path.read_text()).substitute(template_data)
+                to_path.write_text(content)
+            else:
+                shutil.copyfile(path, to_path)
 
     def unpack(self):
         logger.info(f'Generate `{self.name}` dir from templates')
-        for path in self.templates_dir.glob(f'{self.name}/**/*.template'):
-            dst_path = path.relative_to(self.templates_dir).with_suffix('')
-            if not dst_path.exists():
-                dst_path.parent.mkdir(parents=True, exist_ok=True)
-                shutil.copyfile(path, dst_path)
+        for path in self.templates_dir.glob('**/*.template'):
+            self.copy_resource(path=path, to_dir=Path(self.name))
