a9c3883092e75035a48929df1daabe739e6afdd6
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-08-15 09:31:25 +0700

    FEAT: base implementation for simulation

--- tests/e2e/test_example.py
+++ tests/e2e/test_example.py
@@ -8,6 +8,10 @@ def test_example(tester: Tester):
     tester.relaunch_app()
     tester.ui.home.must_not_see_tutorial()
 
+    # Trigger a simulation
+    with tester.simulations.example:
+        pass
+
     # Check elements in settings
     tester.ui.home.go_to_settings()
     tester.ui.settings.swipe('up')
@@ -20,8 +24,3 @@ def test_example(tester: Tester):
     tester.ui.home.textfield('Search Wikipedia').input('Facebook')
     tester.ui.home.element('Social networking service').tap()
     tester.ui.home.button('Back').tap()
-
-
-def test_simulation(tester: Tester):
-    with tester.simulations.example:
-        pass
