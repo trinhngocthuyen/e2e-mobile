fcbee09560edd58bd0f3a870112269038726b48e
Thuyen Trinh <trinhngocthuyen@gmail.com>
2024-06-16 22:20:56 +0800

    FEAT: Support CLI plugins

--- src/e2e/cli.py
+++ src/e2e/cli.py
@@ -1,4 +1,5 @@
 import click
+import pkg_resources
 
 from e2e._cli.demo import main as demo
 from e2e._cli.init import main as init
@@ -15,6 +16,8 @@ main.add_command(demo, name='demo')
 main.add_command(init, name='init')
 main.add_command(new, name='new')
 
+for entry_point in pkg_resources.iter_entry_points('e2e.cli_plugins'):
+    main.add_command(entry_point.load(), entry_point.name)
 
 if __name__ == '__main__':
     main()
